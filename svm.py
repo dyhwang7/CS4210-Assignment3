e33e90cec4778684c3a0e4cb21225047a9022a86
Daeyoung Hwang <45651823+dyhwang7@users.noreply.github.com>
2021-10-17 23:14:16 -0700

    Add files via upload

---
+++ svm.py
@@ -0,0 +1,76 @@
+#-------------------------------------------------------------------------
+# AUTHOR: Daeyoung Hwang
+# FILENAME: svm.py
+# SPECIFICATION: SVM model using various values for c, deg, kernel, and shape
+# FOR: CS 4210- Assignment #3
+# TIME SPENT: how long it took you to complete the assignment
+#-----------------------------------------------------------*/
+
+#IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy OR pandas. You have to work here only with standard vectors and arrays
+
+#importing some Python libraries
+from sklearn import svm
+import csv
+
+dbTest = []
+X_training = []
+Y_training = []
+c = [1, 5, 10, 100]
+degree = [1, 2, 3]
+kernel = ["linear", "poly", "rbf"]
+decision_function_shape = ["ovo", "ovr"]
+highestAccuracy = 0
+
+#reading the data in a csv file
+with open('optdigits.tra', 'r') as trainingFile:
+  reader = csv.reader(trainingFile)
+  for i, row in enumerate(reader):
+      X_training.append(row[:-1])
+      Y_training.append(row[-1])
+
+#reading the data in a csv file
+with open('optdigits.tes', 'r') as testingFile:
+  reader = csv.reader(testingFile)
+  for i, row in enumerate(reader):
+      dbTest.append (row)
+
+#created 4 nested for loops that will iterate through the values of c, degree, kernel, and decision_function_shape
+#--> add your Python code here
+
+for c_item in c: #iterates over c
+    for deg_item in degree: #iterates over degree
+        for ker_item in kernel: #iterates kernel
+           for shape_item in decision_function_shape: #iterates over decision_function_shape
+
+                #Create an SVM classifier that will test all combinations of c, degree, kernel, and decision_function_shape as hyperparameters. For instance svm.SVC(c=1)
+                clf = svm.SVC(C=c_item, degree=deg_item, kernel=ker_item, decision_function_shape=shape_item)
+
+                #Fit SVM to the training data
+                clf.fit(X_training, Y_training)
+
+                #make the classifier prediction for each test sample and start computing its accuracy
+                #--> add your Python code here
+                count = 0
+                for item in dbTest:
+                    class_predicted = clf.predict([item[:-1]])[0]
+                    if class_predicted == item[-1]:
+                        count += 1
+                accuracy = count / len(dbTest)
+
+                if accuracy > highestAccuracy:
+                    highestAccuracy = accuracy
+                    print("Highest SVM accuracy so far: {}, Parameters: a={}, degree={}, kernel= {}, decision_function_shape = {}".format(accuracy, c_item, deg_item, ker_item, shape_item ))
+                #check if the calculated accuracy is higher than the previously one calculated. If so, update update the highest accuracy and print it together with the SVM hyperparameters
+                #Example: "Highest SVM accuracy so far: 0.92, Parameters: a=1, degree=2, kernel= poly, decision_function_shape = 'ovo'"
+                #--> add your Python code here
+
+
+
+
+
+
+
+
+
+
+
